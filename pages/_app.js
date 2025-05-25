3cfcd53d1e0fa550465787fb329368c5ea30352b
ccasson <colincasson80@gmail.com>
2025-05-24 20:38:58 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,22 @@
+// pages/_app.js
 import '../styles/globals.css';
-import Layout from '../components/layout/Layout';
-import { MotionConfig } from 'framer-motion';
-
-// Import react-slick styles
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import GlobalParticles from '../components/common/Particles';
+import Navbar from '../components/layout/Navbar';
+import Footer from '../components/layout/Footer';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <MotionConfig reducedMotion="user">
-      <Layout>
+    <div className="relative min-h-screen bg-brand-dark font-sans overflow-x-hidden">
+      {/* Background particles rendered globally */}
+      <GlobalParticles className="fixed top-0 left-0 w-full h-full z-0" />
+
+      {/* Site layout */}
+      <Navbar />
+      <main className="relative z-10">
         <Component {...pageProps} />
-      </Layout>
-    </MotionConfig>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
