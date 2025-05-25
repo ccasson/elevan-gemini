de0ded06dfe652646ba42760c32040e52c52781b
ccasson <colincasson80@gmail.com>
2025-05-24 20:53:12 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,13 @@
 // pages/_app.js
-import '../styles/globals.css';
+import '@/styles/globals.css';
 import GlobalParticles from '../components/common/Particles';
 import Navbar from '../components/layout/Navbar';
 import Footer from '../components/layout/Footer';
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return (
-    <div className="relative min-h-screen bg-brand-dark font-sans overflow-x-hidden">
-      {/* Background particles rendered globally */}
-      <GlobalParticles className="fixed top-0 left-0 w-full h-full z-0" />
-
-      {/* Site layout */}
+    <div className="relative min-h-screen font-sans">
+      <GlobalParticles className="fixed inset-0 z-0" />
       <Navbar />
       <main className="relative z-10">
         <Component {...pageProps} />
