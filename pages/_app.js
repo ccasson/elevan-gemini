6f3920201a133cc8190e96311043f2b0479f8e16
ccasson <colincasson80@gmail.com>
2025-05-24 20:58:43 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 // pages/_app.js
-import '@/styles/globals.css';
+import React from 'react';
+import '../styles/globals.css';
 import GlobalParticles from '../components/common/Particles';
 import Navbar from '../components/layout/Navbar';
 import Footer from '../components/layout/Footer';
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return (
-    <div className="relative min-h-screen font-sans">
+    <div className="relative overflow-hidden min-h-screen bg-brand-dark font-sans">
       <GlobalParticles className="fixed inset-0 z-0" />
       <Navbar />
       <main className="relative z-10">
